214cacbaad841a6437b8c91ed7ce0400eedcfdbb
Scott Cunningham <cunninsc@tcd.ie>
2013-09-18 00:48:00 +0100

    Added special case where files already exist

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 for i in `/bin/ls ~/src/dotfiles| sed s/"install.sh"//g | xargs`; do
     if [[ $i != $1 ]]; then 
         echo "Linking $i..."
+        if [ -f ~/.$i ]; then
+            mv ~/.$i ~/.$1.old.$RANDOM
+        fi
         ln -s ~/src/dotfiles/$i ~/.$i
     fi
 done
