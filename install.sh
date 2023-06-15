5073d9b9f8b0707ba5e80a3b6ea5054bff3ed305
Scott Cunningham <scottbpc@fb.com>
2023-06-15 04:13:09 -0700

    add fzf

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@
 DIR=$(pwd)
 DATE=$(date +"%Y-%d-%m-%H%M%S")
 
+git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+~/.fzf/install
+
 for i in $(/bin/ls "${DIR}" | grep -v install.sh); do
 	echo "Linking ${i}..."
 	if [ -e "$HOME/.${i}" ]; then
