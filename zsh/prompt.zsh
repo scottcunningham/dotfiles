23e70acf5aac2a17213e27bfc304c11cdc6edff8
Scott Cunningham <scottbpc@fb.com>
2023-06-15 04:18:00 -0700

    powerline prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,16 +1,37 @@
-function powerline_precmd() {
-    PS1="$(~/src/personal/powerline-shell/powerline-shell.py $? --shell zsh 2> /dev/null)"
-}
+prompt scottbpc
 
-function install_powerline_precmd() {
-  for s in "${precmd_functions[@]}"; do
-    if [ "$s" = "powerline_precmd" ]; then
-      return
-    fi
-  done
-  precmd_functions+=(powerline_precmd)
+autoload -U colors && colors
+function parse_hg_branch {
+  if [[ -n $(_scm_prompt) ]]; then
+    # wrap in parens
+    echo "$(_scm_prompt)"
+  fi
 }
+function hgproml {
+  local user="%{$fg[green]%}%n"
+  local at="%{$fg[yellow]%}@"
+  local host="%{$fg[green]%}%m"
 
-if [ "$TERM" != "linux" ]; then
-    install_powerline_precmd
-fi
+  export PS1="$user$at$host%{$fg[yellow]%}:\$(parse_hg_branch)%{$reset_color%} "
+  PS2='> '
+  PS4='+ '
+}
+hgproml
+
+#function powerline_precmd() {
+#    PS1="$(powerline-shell --shell zsh $?)"
+#}
+#
+#function install_powerline_precmd() {
+#  for s in "${precmd_functions[@]}"; do
+#    if [ "$s" = "powerline_precmd" ]; then
+#      return
+#    fi
+#  done
+#  precmd_functions+=(powerline_precmd)
+#}
+#
+#if [ "$TERM" != "linux" ]; then
+#    install_powerline_precmd
+#fi
+#
